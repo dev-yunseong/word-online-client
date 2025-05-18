bd260a4619be8529009e498ebd0674c60171fe3c
Yunseong <dev.yunseong@gmail.com>
2025-05-18 18:16:23 +0900

    feature Destory status 적용

--- Assets/Script/GameScene/ServedObject.cs
+++ Assets/Script/GameScene/ServedObject.cs
@@ -12,6 +12,16 @@ namespace Script.GameScene
                 updatedObjectDto.position.x, 
                 updatedObjectDto.position.y, 
                 0);
+            if (updatedObjectDto.status.Equals("Destroyed"))
+            {
+                ObjectContainer.Instance.UnregisterObject(this);
+                Destroy(gameObject);
+            }
+            else
+            {
+                // TODO - Add Logic for Animation, State, Effect Atc
+                
+            }
         }
     }
 }
