edecfd13a6565258adfedf36313d179c4611b6b9
Yunseong <dev.yunseong@gmail.com>
2025-06-30 16:23:04 +0900

    feature GameObject에 Effect Prefab을 띄어주는 로직 구현

--- Assets/Script/Data/UpdatedObjectDto.cs
+++ Assets/Script/Data/UpdatedObjectDto.cs
@@ -5,4 +5,5 @@ public class UpdatedObjectDto
     public Position position;
     public int hp;
     public string status;
+    public string effect;
 }
--- Assets/Script/GameScene/ServedObject.cs
+++ Assets/Script/GameScene/ServedObject.cs
@@ -5,6 +5,7 @@ namespace Script.GameScene
     public class ServedObject : MonoBehaviour
     {
         public int id;
+        private GameObject _effectInstance = null;
 
         public void UpdateObject(UpdatedObjectDto updatedObjectDto)
         {
@@ -18,10 +19,34 @@ namespace Script.GameScene
                 Destroy(gameObject);
             }
             else
+            // TODO - Add Logic for Animation, State, Effect Atc
+            SetEffect(updatedObjectDto.effect);
+        }
+        
+        private void SetEffect(string effect)
+        {
+            if (effect.Equals("None"))
             {
-                // TODO - Add Logic for Animation, State, Effect Atc
-                
+                if (_effectInstance != null)
+                {
+                    Destroy(_effectInstance);
+                    _effectInstance = null;
+                }
+                return;
             }
+            
+            GameObject effectPrefab = (GameObject) Resources.Load($"Prefabs/Effects/{effect}");
+            if (effectPrefab == null)
+            {
+                Debug.LogWarning($"Effect prefab '{effect}' not found.");
+                return;
+            }
+            if (_effectInstance != null)
+            {
+                Destroy(_effectInstance);
+            }
+            _effectInstance = Instantiate(effectPrefab, transform.position, Quaternion.identity);
+            _effectInstance.transform.SetParent(transform);
         }
     }
 }
